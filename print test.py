f85557bf66fccd7c32b9ecedf8600129d537872d
Matthew Greco <mgreco2112@tutanota.com>
2023-01-27 11:05:15 -0500

    created basic python print tests

---
+++ print test.py
@@ -0,0 +1,11 @@
+# print statement demo
+
+print("standard line")
+
+text = "string variable contents"
+
+print("This is the data stored in the text variable " + text)
+
+print("""Multi
+Line
+Print""")
