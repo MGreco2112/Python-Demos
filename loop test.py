361193d5bb82c7dac3f53d70c377631e27c2ec1a
Matthew Greco <mgreco2112@tutanota.com>
2023-01-28 13:58:10 -0500

    learning python for loops with range, with range for string index, for in, and for each

---
+++ loop test.py
@@ -0,0 +1,30 @@
+# loop tutorial
+
+#basic loop
+for x in range(10):
+    print(x)
+
+#break line
+print()
+
+#index of string utilizing range for condition
+word = "abcdefghij"
+
+for x in range(10):
+    print(word[x])
+
+#break line
+print()
+
+#for in loop
+
+for x in word:
+    print(x)
+
+#break line
+print()
+
+array = [1, 2, 3, 4, 5, 6, 6, 6, 8, 15]
+
+for x in array:
+    print(x)
