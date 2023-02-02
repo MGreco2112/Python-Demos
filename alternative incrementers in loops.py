8565e95cd12e97c463404b8769fa9438efa3309a
Matthew Greco <mgreco2112@tutanota.com>
2023-02-02 18:03:18 -0500

    learning how the python range() function operates

---
+++ alternative incrementers in loops.py
@@ -0,0 +1,12 @@
+#loops with alternative incremeter values
+
+#range object format (START, STOP, STEP)
+#START: integer to begin loop at (default is 0)
+#STOP: termination condition, when counter no longer less than STOP, loop terminates
+#STEP: at what rate the value of count is modified after each iteration (default is 1)
+
+for count in range(0, 10, 2):
+    print(count)
+
+print()
+
