38534b42d1739992c359c238a209b0995915dc64
Matthew Greco <mgreco2112@tutanota.com>
2023-02-02 18:14:23 -0500

    created reverse loop to iterate backwards through string

--- alternative incrementers in loops.py
+++ alternative incrementers in loops.py
@@ -10,3 +10,34 @@ for count in range(0, 10, 2):
 
 print()
 
+#decrimenting loop
+#same format as before, just start is larger than stop with a negative incrementer
+for count in range(10, 0, -1):
+    print(count)
+
+print()
+
+#set string to be reversed
+toReverse = "Backwards"
+#generate new empty string to be built
+output = ""
+
+#print original string for reference
+print(toReverse)
+
+#begin loop, start at last index of string via len(x) - 1, run until index is -1, decrement by a value of 1
+for index in range(len(toReverse) - 1, -1, -1):
+    #store character of string in variable
+    character = toReverse[index]
+
+    #manipulate character casing if either last or first index of string by flipping case
+    if (index == len(toReverse) - 1):
+        character = character.upper()
+    elif (index == 0):
+        character = character.lower()
+
+     #concatonate character to output   
+    output += character
+
+#after loop terminates, print reversed and correctly cased output string
+print(output)
