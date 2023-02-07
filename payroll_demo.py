2d47a4af3015286ffc94692cd0db9120c487dc19
Matthew Greco <mgreco2112@tutanota.com>
2023-02-07 13:57:50 -0500

    updated payroll with multiple employee inputs and formatted display

--- payroll_demo.py
+++ payroll_demo.py
@@ -1,8 +1,28 @@
 #basic, single employee routine
 
-hoursWorked = float(input("Enter hours worked: "))
-payRate = float(input("Enter hourly pay rate: "))
+# hoursWorked = float(input("Enter hours worked: "))
+# payRate = float(input("Enter hourly pay rate: "))
 
-grossWeekly = hoursWorked * payRate
+# grossWeekly = hoursWorked * payRate
 
-print("Employee's Gross Pay is: $" + str(format(grossWeekly, '.2f')))
+# print("Employee's Gross Pay is: $" + str(format(grossWeekly, '.2f')))
+
+#multiple employee payroll
+employees = []
+
+def payroll(hours, rate):
+    return hours * rate
+
+employeeCount = int(input("How many entries would you want to calculate? "))
+
+while len(employees) < employeeCount:
+    name = input("Enter Employee name: ")
+    hoursWorked = float(input("Enter hours worked: "))
+    payRate = float(input("Enter hourly pay rate: "))
+
+    gross = payroll(hoursWorked, payRate)
+
+    employees.append([name, gross])
+
+for x in range(len(employees)):
+    print(employees[x][0] + ": $" + str(format(employees[x][1], '.2f')))
