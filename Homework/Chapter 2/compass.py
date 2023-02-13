f62a94636a85f7c8f298a2b3ae2e66d1914f385d
Matthew Greco <mgreco2112@tutanota.com>
2023-02-13 18:58:27 -0500

    finished drawing adjacent filled diamonds

--- Homework/Chapter 2/compass.py
+++ Homework/Chapter 2/compass.py
@@ -1,6 +1,11 @@
 #import Turtle module
 import turtle
+#import Time module for waiting
+import time
 
+#Compass Drawing
+#set background color to blue
+turtle.bgcolor("gray")
 #lift pen
 turtle.penup()
 #move turtle to position 0 x 400 px
@@ -51,6 +56,64 @@ turtle.goto(210, -7)
 turtle.write("EAST")
 #move position to 0 x 0
 turtle.goto(0,0)
+
+#wait before moving to next drawing
+time.sleep(3)
+
+#clear drawing, pen and fill settings, and reset turtle location, but not background color
+turtle.reset()
+
+#Adjacent Diamonds
+#lift pen
+turtle.penup()
+#position turtle at 100 x 100 px
+turtle.goto(-100, 100)
+#lower pen
+turtle.pendown()
+#set fill color
+turtle.fillcolor("white")
+#activate filling
+turtle.begin_fill()
+#turn turtle right by 130
+turtle.right(135)
+#move turtle forward by 100
+turtle.forward(100)
+#turn turtle left by 90
+turtle.left(90)
+#move turtle forward 100
+turtle.forward(100)
+#turn turtle left by 90
+turtle.left(90)
+#move turtle forward by 100
+turtle.forward(100)
+#store location in variable
+turtlePosition = turtle.pos()
+#turn turtle left by 90
+turtle.left(90)
+#move turtle forward 100
+turtle.forward(100)
+#end filling
+turtle.end_fill()
+#position turtle at location saved in turtlePosition
+turtle.goto(turtlePosition[0], turtlePosition[1])
+#turn turtle right by 90
+turtle.right(90)
+#begin fill
+turtle.begin_fill()
+#loop to repeat steps
+for x in range(4):
+    #move turtle forward 100
+    turtle.forward(100)
+    #turn turtle right 90
+    turtle.right(90)
+#fill in shape
+turtle.end_fill()
+
+#wait three seconds before drawing next piece
+time.sleep(3)
+
+#clear screen and reset turtle position, keeps background color
+turtle.reset()
 
 #keep window open after reaching last line
 turtle.done()
