740c79ed6903ac35931d6f81965c51a70af06e20
Matthew Greco <mgreco2112@tutanota.com>
2023-02-23 18:59:17 -0500

    created logic flow for if/else chain

---
+++ if else.py
@@ -0,0 +1,10 @@
+#if else control flow in class demonstration
+
+weather = input("Is it cold outside? True or False: ")
+
+if (weather == "True"):
+    print("Wear a coat.")
+    print("Wear a hat.")
+    print("Wear gloves.")
+else:
+    print("Go Swimming!")
