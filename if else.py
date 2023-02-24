79fc9b4908f82ba2ede588596d1e79d39cb26f36
Matthew Greco <mgreco2112@tutanota.com>
2023-02-23 21:03:02 -0500

    added string comparator statements without elif

--- if else.py
+++ if else.py
@@ -1,11 +1,22 @@
 #if else control flow in class demonstration
 
-temperature_farenheit = float(input("Enter the temperature in Farenheit: "))
+# temperature_farenheit = float(input("Enter the temperature in Farenheit: "))
 
 
-if temperature_farenheit <= 60:
-    print("Wear a coat.")
-    print("Wear a hat.")
-    print("Wear gloves.")
+# if temperature_farenheit <= 60:
+#     print("Wear a coat.")
+#     print("Wear a hat.")
+#     print("Wear gloves.")
+# else:
+#     print("Go Swimming!")
+
+name1 = input("Enter a name: ")
+name2 = input("Enter another name: ")
+
+if name1 == name2:
+    print(f"The names are both {name1}!")
 else:
-    print("Go Swimming!")
+    if name1 > name2:
+        print(f"{name2} comes before {name1}")
+    else:
+        print(f"{name1} comes before {name2}")
