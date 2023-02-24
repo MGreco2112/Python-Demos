03c9a82590104aef3604053d9b1dd551c28b050c
Matthew Greco <mgreco2112@tutanota.com>
2023-02-23 19:18:53 -0500

    changed input variable value to a True boolean value, altered if conditional to reflect change

--- if else.py
+++ if else.py
@@ -1,8 +1,9 @@
 #if else control flow in class demonstration
 
-weather = input("Is it cold outside? True or False: ")
+# weather = input("Is it cold outside? True or False: ")
+weather = True
 
-if (weather == "True"):
+if (weather):
     print("Wear a coat.")
     print("Wear a hat.")
     print("Wear gloves.")
