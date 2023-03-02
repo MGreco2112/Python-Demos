579ff297c6071e4b7223300f1c1d0efb37978bae
Matthew Greco <mgreco2112@tutanota.com>
2023-03-02 18:58:09 -0500

    added comments to the if-else model

--- grading scale.py
+++ grading scale.py
@@ -18,16 +18,15 @@ grade = float(input("Enter your current grade: "))
 
 if grade >= D_START_RANGE: #if grade is greater than F range, enter evaluation block for specific grade
     if grade >= A_START_RANGE: #if grade within A Range
-        print("A")
-    else:
-        if grade >= B_START_RANGE:
-            print("B")
-        else:
-            if grade >= C_START_RANGE:
-                print("C")
-            else:
-                if grade >= D_START_RANGE:
-                    print("D")
+        print("A") #print grade
+    else: #else, if not within A Range
+        if grade >= B_START_RANGE: #if grade within B Range
+            print("B") #print grade
+        else: #else, if not within B Range
+            if grade >= C_START_RANGE: #if grade within C Range
+                print("C") #print grade
+            else: #else if not within C Range
+                    print("D") #print D grage
 else: #otherwise print F grade
     print("F")
 
