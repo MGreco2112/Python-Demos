1f7c70de4149bc6add7966aeee46211301353f9d
Matthew Greco <mgreco2112@tutanota.com>
2023-03-20 12:00:41 -0400

    solved Chapter 4 Exersice 3

---
+++ Greco_Matthew_Ch4_ex3.py
@@ -0,0 +1,6 @@
+#write a for loop that will display the numbers 0 - 1000, step increments by 10
+
+#define for loop
+for i in range(0, 1001, 10): #range arguments: 0 is START, 1001 is condition for < comparison, 10 is STEP incrementation
+    #print value of i counter
+    print(i)
