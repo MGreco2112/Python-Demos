c2285ca89a43dfcbd4600c74d48c5c12632322ad
Matthew Greco <mgreco2112@tutanota.com>
2023-03-20 12:19:32 -0400

    finished Exercise 6

---
+++ Greco_Matthew_Ch4_ex6.py
@@ -0,0 +1,27 @@
+#rewrite statements using augmented assignment operators
+
+#original statement
+#x = x + 1
+
+#initialize x var to 0
+x = 0
+#increment x value by 1
+x += 1
+
+#original statement
+#x = x * 2
+
+#assign the value of x to x's current value times 2
+x *= 2
+
+#original statement
+#x = x / 10
+
+#divide current value of x by value of 10, assign to x
+x /= 10
+
+#origianl statement
+#x = x - 100
+
+#assign x's current value to that of x - 100
+x -= 100
