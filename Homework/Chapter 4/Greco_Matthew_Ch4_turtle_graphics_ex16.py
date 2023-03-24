a4ee0f176ca2d44e27fb001ca4047c6d2bfabd98
Matthew Greco <mgreco2112@tutanota.com>
2023-03-24 13:41:35 -0400

    furnished code with comments

--- Homework/Chapter 4/Greco_Matthew_Ch4_turtle_graphics_ex16.py
+++ Homework/Chapter 4/Greco_Matthew_Ch4_turtle_graphics_ex16.py
@@ -1,31 +1,32 @@
 # utilize looping structure to create squares (two lines at 90 degree angle, using turtle window as other two lines)
 
+#import turtle
 import turtle
 
-SQUARE_COUNT = 150
-DRAW_SPEED = 0
-SQUARE_GAP = 5
-length = 5
-NORTH = 90
-SOUTH = 270
-WEST = 180
-EAST = 0
-x_cor = 370
-Y_COR = -315
+SQUARE_COUNT = 150 #define var for number of loops iterated
+DRAW_SPEED = 0 #draw speed variable
+SQUARE_GAP = 5 #distance modifier for turtle's x coordinate
+length = 5 #initial length of square sides
+NORTH = 90 # heading for North
+EAST = 0 # heading for East
+x_cor = 370 #initial x coordinate starting location
+Y_COR = -315 #initial y coordinate starting location
 
+#set draw speed for fast drawing
 turtle.speed(DRAW_SPEED)
 
-for x in range(SQUARE_COUNT):
-    turtle.penup()
-    turtle.goto(x_cor, Y_COR)
-    turtle.pendown()
-    turtle.setheading(NORTH)
-    turtle.forward(length)
-    turtle.setheading(0)
-    turtle.forward(length)
-    x_cor -= SQUARE_GAP
-    length += SQUARE_GAP
+#initialize for loop over range of square count
+for squ in range(SQUARE_COUNT):
+    turtle.penup() #lift pen
+    turtle.goto(x_cor, Y_COR) #set turtle to location of x and y
+    turtle.pendown() #drop pen
+    turtle.setheading(NORTH) #set heading north
+    turtle.forward(length) #draw line current length of square
+    turtle.setheading(EAST) #set heading east
+    turtle.forward(length) #draw line again
+    x_cor -= SQUARE_GAP #modify x coordinate variable by square gap
+    length += SQUARE_GAP #modify length variable by square gap
 
 
 
-turtle.done()
+turtle.done() #display window post code execution
