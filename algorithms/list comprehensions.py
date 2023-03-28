0be85f9d92a7af7f826ba12303458f5ccf3cd033
Matthew Greco <mgreco2112@tutanota.com>
2023-03-28 06:50:08 -0400

    after solving with nested loop, comprehension example was researched. I provided comments based on my observations of the code and utilizations within the IDE

--- algorithms/list comprehensions.py
+++ algorithms/list comprehensions.py
@@ -4,12 +4,20 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    output = [] #initialize output array
+    # output = [] #initialize output array
     
-    for outer in range(x + 1):
-        for middle in range(y + 1):
-            for inner in range(z + 1): #define triple nested loop for each permutation of coordinates
-                if outer + middle + inner != n: #test sum of coordinates so that they are not equal to test number
-                    output.append([outer, middle, inner]) #append subarray to output array
+    # for outer in range(x + 1):
+    #     for middle in range(y + 1):
+    #         for inner in range(z + 1): #define triple nested loop for each permutation of coordinates
+    #             if outer + middle + inner != n: #test sum of coordinates so that they are not equal to test number
+    #                 output.append([outer, middle, inner]) #append subarray to output array
     
-    print(output) #print output array
+    # print(output) #print output array
+
+    # List Comprehension example for understanding of desired output
+
+    #1) define multiple variables
+    #2) create for loops with these iterable variables using ranges of coordinates plus 1 for <= conditional
+    #3) utilize if statement to check conditional for comprehension case
+    #4) wrap within print statement so coordinates are printed within an array inside of comprehension array
+    print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i+j+k != n])
