a1c59ab7ff4f3d0d0b1b41e1de5243399afe1965
Matthew Greco <mgreco2112@tutanota.com>
2023-03-28 06:42:27 -0400

    added comments to solved problem

--- algorithms/list comprehensions.py
+++ algorithms/list comprehensions.py
@@ -1,15 +1,15 @@
 if __name__ == '__main__':
-    x = int(input())
+    x = int(input()) #take inputs from system for three coordinates and one test number
     y = int(input())
     z = int(input())
     n = int(input())
     
-    output = []
+    output = [] #initialize output array
     
     for outer in range(x + 1):
         for middle in range(y + 1):
-            for inner in range(z + 1):
-                if outer + middle + inner != n:
-                    output.append([outer, middle, inner])
+            for inner in range(z + 1): #define triple nested loop for each permutation of coordinates
+                if outer + middle + inner != n: #test sum of coordinates so that they are not equal to test number
+                    output.append([outer, middle, inner]) #append subarray to output array
     
-    print(output)
+    print(output) #print output array
