917122fcbafa41a06461f6514cd4c92e521e578b
Matthew Greco <mgreco2112@tutanota.com>
2023-03-30 17:27:15 -0400

    modified solution for effeciency, added comments

--- algorithms/String Validator.py
+++ algorithms/String Validator.py
@@ -1,6 +1,7 @@
 if __name__ == '__main__':
     s = input()
     
+    #initialize boolean variables to hold conditions based on characters in String s, initlze to False
     is_alnum = False
     is_alpha = False
     has_digits = False
@@ -8,20 +9,25 @@ if __name__ == '__main__':
     has_upper = False
     
     
+    #iterate through each character in string s
     for ch in s:
-        if ch.isalnum():
-            is_alnum = True
+        if is_alnum == False and ch.isalnum(): #test if current character is alpha numeric
+            is_alnum = True #set bool to True if condition is encountered
         
-        if ch.isalpha():
-            is_alpha = True
+        if is_alpha == False and ch.isalpha(): #test if current character is alphabetical character
+            is_alpha = True #set bool to True if condition is encountered
         
-        if ch.isdigit():
-            has_digits = True
+        if has_digits == False and ch.isdigit(): #test if current character is digit
+            has_digits = True #set bool to True if condition is encountered
             
-        if ch.islower():
-            has_lower = True
+        if has_lower == False and ch.islower(): #test if current character is lowercase
+            has_lower = True #set bool to True if condition is encountered
             
-        if ch.isupper():
-            has_upper = True
+        if has_upper == False and ch.isupper(): #test if current character is uppercase
+            has_upper = True #set bool to True if condition is encountered
     
-    print(f"{is_alnum}\n{is_alpha}\n{has_digits}\n{has_lower}\n{has_upper}")
+        #test if all boolean values are true, indicating no further testing is required
+        if is_alnum == True and is_alpha == True and has_digits == True and has_lower == True and has_upper == True:
+            break #if no further testing is required, break from loop and print results
+    
+    print(f"{is_alnum}\n{is_alpha}\n{has_digits}\n{has_lower}\n{has_upper}") #print boolean values in order line separated
