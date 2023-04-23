f00b35ea8f24917424f579b98547e2c56e1a03bc
Matthew Greco <mgreco2112@tutanota.com>
2023-04-23 13:14:43 -0400

    modified for debugging purposes. pointer is not being reassigned

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -3,12 +3,21 @@
 def bubble_sort(arr):
     output = arr
 
-    for _ in range(len(output) - 1):
-        if output[_] > output[_ + 1]:
-            temp = output[_]
-            output[_] = output[_ + 1]
-            output[_ + 1] = temp
-            _ = 0
+    reset = False
+
+    for pointer in range(len(output) - 1):
+
+        print(pointer)
+        if output[pointer] > output[pointer + 1]:
+            temp = output[pointer]
+            output[pointer] = output[pointer + 1]
+            output[pointer + 1] = temp
+            reset = True
+        
+        if reset:
+            pointer = 0
+            print(pointer)
+            
     
     return output
 
