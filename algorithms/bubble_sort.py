200c3aba2e89ea9c072c41695760f5359c5d2476
Matthew Greco <mgreco2112@tutanota.com>
2023-04-24 12:30:24 -0400

    solved bubble sort algorithm in python

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,26 +1,28 @@
 #basic bubble sort array function
 
-def bubble_sort(arr):
-    output = arr
+def bubble_sort(arr): #define bubble sort function that takes one argument
 
-    reset = False
+    swappedElements = True #initilize swappedElements boolean value to true, for use with the while loop
 
-    for pointer in range(len(output) - 1):
-
-        print(pointer)
-        if output[pointer] > output[pointer + 1]:
-            temp = output[pointer]
-            output[pointer] = output[pointer + 1]
-            output[pointer + 1] = temp
-            reset = True
+    while swappedElements: #define while loop referencing swappedElements for a condition
+        swappedThisCycle = False #initialize swappedThisCycle boolean to False to monitor the progress of each iteration through the list
+        for element in range(len(arr) - 1): #define for loop through range of the length of the array minus 1, to allow for looking at the coming index
+            if arr[element] > arr[element + 1]: #if current element is greater than next element
+                temp = arr[element] #initialize temp var to current element
+                arr[element] = arr[element + 1] #change element at index of current element to next element
+                arr[element + 1] = temp #change element at index of next element to stored temp variable
+                swappedThisCycle = True #set swappedThisCycle to true, indicating that a change was made during this iteration
         
-        if reset:
-            pointer = 0
-            print(pointer)
+        if not swappedThisCycle: #if no change was made this cycle
+            swappedElements = False #change conditional var in while loop to false, breaking loop
             
     
-    return output
+    return arr #return modified array
 
-test = [5, 4, 3, 2, 1]
+test = [5, 4, 3, 2, 1] #initialize test var to array not in numericl order (lowest to greatest)
 
-print(bubble_sort(test))
+print(bubble_sort(test)) #print the result of bubble sort on test
+
+print(test) #print current value of test after bubble sort algorithm has run. This shows that this is a "destructive" algorithm
+#the modifications done to the input data reflect the original data, not just the return value. I will find a way to perform these operations
+#without destorying the original data set.
